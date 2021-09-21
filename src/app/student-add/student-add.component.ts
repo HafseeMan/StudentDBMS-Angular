6c85aead7d67e77b1d24dd710e86e685447673e4
HafseeMan <hafoosah1@gmail.com>
2021-09-21 16:33:52 +0100

    second commit

--- src/app/student-add/student-add.component.ts
+++ src/app/student-add/student-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RestApiService } from '../rest-api.service.ts.service';
 
@@ -11,19 +12,36 @@ export class StudentAddComponent implements OnInit {
 
   @Input() studentDetails = { id: 0 , fname: '', sname: '', gender: '', course: ''}
 
+  addForm = new FormGroup({
+    id : new FormControl('', Validators.required),
+    course : new FormControl('', Validators.required),
+    fname : new FormControl('', Validators.required),
+    sname : new FormControl('', Validators.required),
+    gender : new FormControl('', Validators.required)
+  });
+
  constructor(
-   public  restApi: RestApiService, /**containing http request methods */
+   public  restApi: RestApiService, //containing http request methods */
    public router: Router
  ) { }
 
   ngOnInit(): void {
   }
 
+  formError = false;
+
   addStudent(){ /**addStudent(dataStudent) */
-    this.restApi.createStudent(this.studentDetails).subscribe(
-      (data:{}) => {
-        console.log("should work " + data)
-      }
-    )
+
+    if (this.addForm.invalid){
+      this.formError = true;
+    }
+    else{
+      this.restApi.createStudent(this.studentDetails).subscribe(
+        (data:{}) => {
+          location.reload()
+        }
+      )  
+    }
+    
   }
 }
